1b5be9d8e7e5f67dd8d7a7ac8a7a84f041f34cf2
gutterbacon <gutterbacon@protonmail.com>
2020-09-29 04:37:28 +0000

    move flutter example to main, fixes makefile for path

---
+++ sys-account/main/cli/cli.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/getcouragenow/sys/sys-account/server/rpc/v2"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "auth",
+	Short: "auth cli",
+}
+
+func main() {
+	rootCmd.AddCommand(rpc.AuthServiceClientCommand())
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("command failed: %v", err)
+	}
+}
