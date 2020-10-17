ee5d6dd50e3bd26a58cf1a504f464bed5dd3f21b
gutterbacon <gutterbacon@protonmail.com>
2020-10-17 13:19:05 +0000

    removed sys-core/service/flu

--- sys-core/service/flu/lib/src/settings/core_settings.dart
+++
@@ -1,3 +0,0 @@
-export 'package:sys_core/src/settings/src/module_config.dart';
-export 'package:sys_core/src/settings/src/module_config_item.dart';
-export 'package:sys_core/src/settings/src/core_settings_service.dart';
